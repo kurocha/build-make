30484395cf630c34ee757c90f015b23a036b85b1
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2017-07-11 01:43:19 +1200

    Use Etc.nprocessors

--- teapot.rb
+++ teapot.rb
@@ -3,6 +3,8 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
+require 'etc'
+
 teapot_version "1.0.0"
 
 define_target "build-make" do |target|
@@ -31,7 +33,7 @@ define_target "build-make" do |target|
 			apply do |arguments|
 				destination_prefix = arguments[:prefix]
 				
-				run!("make", "-j", System::CPU.count.to_s, chdir: destination_prefix)
+				run!("make", "-j", Etc.nprocessors.to_s, chdir: destination_prefix)
 				run!("make", "install", chdir: destination_prefix)
 				
 				Array(arguments[:package_files]).each do |path|
